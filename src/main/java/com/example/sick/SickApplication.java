da4cf5870af966a13dbab3b0b2ff3b137b6f93b5
Edvinas <edvinas.gerdvila@mif.stud.vu.lt>
2024-04-09 17:06:57 +0300

    Initiated basic structure for backend

---
+++ src/main/java/com/example/sick/SickApplication.java
@@ -0,0 +1,13 @@
+package com.example.sick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SickApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SickApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sick/SickApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SickApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
