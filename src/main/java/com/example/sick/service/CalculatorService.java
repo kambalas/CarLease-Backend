bbede12e811b6f0ddebca192b57ba38fbfd6d592
37060 <patricija.katinaite@gmail.com>
2024-04-16 15:01:52 +0300

    fixed calculator formula

--- src/main/java/com/example/sick/service/CalculatorService.java
+++ src/main/java/com/example/sick/service/CalculatorService.java
@@ -29,30 +29,28 @@ public class CalculatorService {
                 calculatorDAOResponse.eco() :
                 calculatorDAOResponse.regular());
 
-        BigDecimal rate = interestRate.divide(BigDecimal.valueOf(100));
+        BigDecimal rate = interestRate.divide(BigDecimal.valueOf(100), MathContext.DECIMAL128);
 
-        BigDecimal monthlyRate = rate.divide(BigDecimal.valueOf(12), MathContext.DECIMAL64);
+        BigDecimal residualValue = calculatorRequest.carValue()
+                .multiply(BigDecimal.valueOf(calculatorRequest.residualValuePercentage()))
+                .divide(BigDecimal.valueOf(100), MathContext.DECIMAL128);
 
-        BigDecimal loanAmountMinusDownPaymentAndResidual = calculatorRequest.carValue()
-                .subtract(calculatorRequest.downPayment())
-                .subtract(calculateResidualValue(calculatorRequest.carValue(), calculatorRequest.residualValuePercentage()));
+        BigDecimal loanAmountMinusDownPayment = calculatorRequest.carValue()
+                .subtract(calculatorRequest.downPayment());
 
-        BigDecimal totalPayableInterest = loanAmountMinusDownPaymentAndResidual
-                .multiply(monthlyRate)
+        BigDecimal totalPayableInterest = loanAmountMinusDownPayment
+                .multiply(rate)
                 .multiply(BigDecimal.valueOf(calculatorRequest.period()))
-                .divide(BigDecimal.valueOf(12), 2, RoundingMode.HALF_UP);
+                .divide(BigDecimal.valueOf(12), MathContext.DECIMAL128);
 
-        BigDecimal monthlyInterest = totalPayableInterest.divide(BigDecimal.valueOf(calculatorRequest.period()), 2, RoundingMode.HALF_UP);
+        BigDecimal monthlyInterest = totalPayableInterest.divide(BigDecimal.valueOf(calculatorRequest.period()), MathContext.DECIMAL128);
 
-        BigDecimal monthlyLoanAmount = loanAmountMinusDownPaymentAndResidual
-                .divide(BigDecimal.valueOf(calculatorRequest.period()), 2, RoundingMode.HALF_UP);
+        BigDecimal monthlyLoanAmount = loanAmountMinusDownPayment
+                .subtract(residualValue)
+                .divide(BigDecimal.valueOf(calculatorRequest.period()), MathContext.DECIMAL128);
 
-        return new CalculatorResponse(monthlyLoanAmount.add(monthlyInterest));
+        BigDecimal monthlyPayment = monthlyLoanAmount.add(monthlyInterest);
+
+        return new CalculatorResponse(monthlyPayment.setScale(2, RoundingMode.HALF_UP));
     }
-
-    private BigDecimal calculateResidualValue(BigDecimal carValue, int residualValuePercentage) {
-        BigDecimal percentage = BigDecimal.valueOf(residualValuePercentage).divide(BigDecimal.valueOf(100));
-        return carValue.multiply(percentage);
-    }
-
 }
