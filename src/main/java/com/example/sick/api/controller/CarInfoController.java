18b1609fdb123771e1396df854352a8307d727c3
Gerdvila <edvinas.gerdvila@mif.stud.vu.lt>
2024-04-23 13:28:23 +0300

    Update CarInfoController.java

--- src/main/java/com/example/sick/api/controller/CarInfoController.java
+++ src/main/java/com/example/sick/api/controller/CarInfoController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/user/carApi")
 public class CarInfoController {
 
     private final CarInfoService carInfoService;
