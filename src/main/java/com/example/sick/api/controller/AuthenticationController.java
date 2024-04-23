92cc097db6126e9ee0ff517547206b5b7be948e1
Motiejūnas Ugnius <motiejunasugnius@gmail.com>
2024-04-23 10:11:47 +0300

    fixed return type

--- src/main/java/com/example/sick/api/controller/AuthenticationController.java
+++ src/main/java/com/example/sick/api/controller/AuthenticationController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -23,16 +24,15 @@ public class AuthenticationController {
 
   @PreAuthorize("hasRole('ROLE_Admin')")
   @PostMapping("/register")
-  public ResponseEntity<AuthenticationDAOResponse> register(
-          @RequestBody RegisterDAORequest request
-  ) {
-    return new ResponseEntity<>(authenticationService.register(request), HttpStatus.OK);
+  @ResponseStatus(HttpStatus.OK)
+  public AuthenticationDAOResponse register(@RequestBody RegisterDAORequest request) {
+    return authenticationService.register(request);
   }
+
   @CrossOrigin(origins = {"http://localhost:4200", "https://ci-cd-angular.onrender.com"})
   @PostMapping("/authenticate")
-  public ResponseEntity<AuthenticationDAOResponse> authenticate(
-          @RequestBody AuthenticationDAORequest request
-  ) {
-    return new ResponseEntity<>(authenticationService.authenticate(request), HttpStatus.OK);
+  @ResponseStatus(HttpStatus.OK)
+  public AuthenticationDAOResponse authenticate(@RequestBody AuthenticationDAORequest request) {
+    return authenticationService.authenticate(request);
   }
 }
