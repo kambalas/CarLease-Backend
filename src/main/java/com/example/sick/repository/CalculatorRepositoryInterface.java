fcf84b5d8bf92576fdb992cd05425ce8b1a68095
Motiejūnas Ugnius <motiejunasugnius@gmail.com>
2024-05-02 13:11:16 +0300

    tests

--- src/main/java/com/example/sick/repository/CalculatorRepositoryInterface.java
+++ src/main/java/com/example/sick/repository/CalculatorRepositoryInterface.java
@@ -2,6 +2,8 @@ package com.example.sick.repository;
 
 import com.example.sick.domain.CalculatorDAOResponse;
 
+import java.math.BigDecimal;
+
 public interface CalculatorRepositoryInterface {
 
     CalculatorDAOResponse selectAllInterestRate();
